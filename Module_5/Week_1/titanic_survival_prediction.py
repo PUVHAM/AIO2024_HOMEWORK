85bb6e61f80031d465f97a87725548358a5e7875
PUVHAM <phamquangvu19082005@gmail.com>
2024-11-02 15:39:50 -0700

    feat: add function in titanic_survival_prediction.py to avoid duplicate

--- Module_5/Week_1/titanic_survival_prediction.py
+++ Module_5/Week_1/titanic_survival_prediction.py
@@ -3,8 +3,7 @@ from visualize import plot_figures
 from load_dataset import load_df, split_dataset
 from logistic_regression import LogisticRegression
 
-if __name__ == "__main__":
-    csv_name = "Titanic"
+def run_experiment(csv_name):
     x, y = load_df(csv_name, DatasetConfig.DATASET_PATH.get(csv_name))
     x_train, y_train, x_test, y_test, x_val, y_val = split_dataset(x, y)
     regression = LogisticRegression(x_train, y_train, x_val, y_val, csv_name)
@@ -14,6 +13,8 @@ if __name__ == "__main__":
     val_set_acc = regression._compute_accuracy(x_val, y_val, theta)
     test_set_acc = regression._compute_accuracy(x_test, y_test, theta)
     print('Evaluation on validation and test set:')
-    print(f'Accuracy: {val_set_acc}')
-    print(f'Accuracy: {test_set_acc}')
+    print(f'Validation Accuracy: {val_set_acc}')
+    print(f'Test Accuracy: {test_set_acc}')
     
+if __name__ == "__main__":
+    run_experiment("Titanic")    
