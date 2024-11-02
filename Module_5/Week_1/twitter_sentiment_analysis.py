f059ff5859292492aa263b89ad65c166ec88341a
PUVHAM <phamquangvu19082005@gmail.com>
2024-11-02 15:40:30 -0700

    feat: use function in twitter_sentiment_analysis.py to avoid duplicate

--- Module_5/Week_1/twitter_sentiment_analysis.py
+++ Module_5/Week_1/twitter_sentiment_analysis.py
@@ -1,19 +1,4 @@
-from config import DatasetConfig
-from visualize import plot_figures
-from load_dataset import load_df, split_dataset
-from logistic_regression import LogisticRegression
-
+from titanic_survival_prediction import run_experiment 
+    
 if __name__ == "__main__":
-    csv_name = "Sentiment"
-    x, y = load_df(csv_name, DatasetConfig.DATASET_PATH.get(csv_name))
-    x_train, y_train, x_test, y_test, x_val, y_val = split_dataset(x, y)
-    regression = LogisticRegression(x_train, y_train, x_val, y_val, csv_name)
-    train_losses, val_losses, train_accs, val_accs, theta = regression.train()
-    plot_figures(train_losses, val_losses, train_accs, val_accs)
-    
-    val_set_acc = regression._compute_accuracy(x_val, y_val, theta)
-    test_set_acc = regression._compute_accuracy(x_test, y_test, theta)
-    print('Evaluation on validation and test set:')
-    print(f'Accuracy: {val_set_acc}')
-    print(f'Accuracy: {test_set_acc}')
-    
+    run_experiment("Sentiment")    
