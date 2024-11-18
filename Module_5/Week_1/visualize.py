c195c872dbfbdee7245c6f588956498700523d89
PUVHAM <phamquangvu19082005@gmail.com>
2024-11-18 11:59:57 -0800

    feat: add customizable metric parameter in plot_figures function

--- Module_5/Week_1/visualize.py
+++ Module_5/Week_1/visualize.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-def plot_figures(train_losses, val_losses, train_accs, val_accs):
+def plot_figures(train_losses, val_losses, train_accs, val_accs, score_type="Accuracy"):
     _, ax = plt.subplots(2, 2, figsize=(12, 10))
 
     ax[0, 0].plot(train_losses)
@@ -15,13 +15,13 @@ def plot_figures(train_losses, val_losses, train_accs, val_accs):
 
     ax[1, 0].plot(train_accs)
     ax[1, 0].set_xlabel('Epoch')
-    ax[1, 0].set_ylabel('Accuracy')
-    ax[1, 0].set_title('Training Accuracy')
+    ax[1, 0].set_ylabel(f'{score_type}')
+    ax[1, 0].set_title(f'Training {score_type}')
 
     ax[1, 1].plot(val_accs, color='orange')
     ax[1, 1].set_xlabel('Epoch')
-    ax[1, 1].set_ylabel('Accuracy')
-    ax[1, 1].set_title('Validation Accuracy')
+    ax[1, 1].set_ylabel(f'{score_type}')
+    ax[1, 1].set_title(f'Validation {score_type}')
 
     plt.tight_layout()  
     plt.show()
